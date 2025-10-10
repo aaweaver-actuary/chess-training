40376fd422057d9a58a344a1e3082d24696fc3a3
= <=>
2025-10-10 10:22:39 -0400

    asdf

--- web-ui/src/utils/formatUnlockDate.ts
+++ web-ui/src/utils/formatUnlockDate.ts
@@ -14,8 +14,9 @@ export function formatUnlockDate(
   input: string,
   options: DateFormatterOptions = defaultOptions,
 ): string {
-  const normalizedInput =
-    /^\d{4}-\d{2}-\d{2}$/.test(input) ? `${input}T00:00:00Z` : input;
+  const normalizedInput = /^\d{4}-\d{2}-\d{2}$/.test(input)
+    ? `${input}T00:00:00Z`
+    : input;
 
   const parsed = new Date(normalizedInput);
 
@@ -24,7 +25,8 @@ export function formatUnlockDate(
   }
 
   // Default to UTC so that date-only strings are not shifted by the local timezone.
-  return new Intl.DateTimeFormat(undefined, { timeZone: 'UTC', ...options }).format(
-    parsed,
-  );
+  return new Intl.DateTimeFormat(undefined, {
+    timeZone: 'UTC',
+    ...options,
+  }).format(parsed);
 }
